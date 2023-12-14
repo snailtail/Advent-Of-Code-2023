ebbd8e68b30c5201ac7b40f27e3ad4f8986e07f0
Magnus Olofsson <magnusolofsson@outlook.com>
2023-12-14 17:13:24 +0100

    Solved part 2

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,13 +1,14 @@
 ﻿
 
 
-string[] map = File.ReadAllLines("../data/10.dat");
+
+char[][] mapData = File.ReadAllLines("../data/10.dat").Select(l => l.ToCharArray()).ToArray();
 int Sx = 0, Sy = 0;
-for (int y = 0; y < map.Length; y++)
+for (int y = 0; y < mapData.Length; y++)
 {
-    for (int x = 0; x < map[0].Length; x++)
+    for (int x = 0; x < mapData[0].Length; x++)
     {
-        if (map[y][x] == 'S')
+        if (mapData[y][x] == 'S')
         {
             Sx = x;
             Sy = y;
@@ -15,11 +16,15 @@ for (int y = 0; y < map.Length; y++)
     }
 }
 
-Console.WriteLine($"Part 1: {GetSteps(map, Sx, Sy)}");
 
-int GetSteps(string[] Map, int startX, int startY)
+Console.WriteLine($"Part 1: {Solve(mapData, Sx, Sy)}");
+var part2 = new PartTwo();
+part2.Solve();
+
+int Solve(char[][] m, int startX, int startY)
 {
 
+    HashSet<char> Possible_S = new() { '|', '-', 'J', 'L', 'F', '7' };
     Queue<(int, int)> q = new();
     HashSet<(int, int)> Visited = new();
     char[] north = { 'S', '|', 'L', 'J' };
@@ -33,35 +38,42 @@ int GetSteps(string[] Map, int startX, int startY)
     {
         (int y, int x) = q.Dequeue();
         // go north
-        if (y > 0 && !Visited.Contains((y - 1, x)) && north.Any(c => c == Map[y][x]) && south.Any(c => c == Map[y - 1][x]))
+        if (y > 0 && !Visited.Contains((y - 1, x)) && north.Any(c => c == m[y][x]) && south.Any(c => c == m[y - 1][x]))
         {
             Visited.Add((y - 1, x));
             q.Enqueue((y - 1, x));
+           
+
         }
 
         // go south
-        if (y < Map.Length - 1 && !Visited.Contains((y + 1, x)) && south.Any(c => c == Map[y][x]) && north.Any(c => c == Map[y + 1][x]))
+        if (y < m.Length - 1 && !Visited.Contains((y + 1, x)) && south.Any(c => c == m[y][x]) && north.Any(c => c == m[y + 1][x]))
         {
             Visited.Add((y + 1, x));
             q.Enqueue((y + 1, x));
+           
         }
 
         // go west
-        if (x > 0 && !Visited.Contains((y, x - 1)) && west.Any(c => c == Map[y][x]) && east.Any(c => c == Map[y][x - 1]))
+        if (x > 0 && !Visited.Contains((y, x - 1)) && west.Any(c => c == m[y][x]) && east.Any(c => c == m[y][x - 1]))
         {
             Visited.Add((y, x - 1));
             q.Enqueue((y, x - 1));
+           
         }
 
         // go east
-        if (x < Map[0].Length - 1 && !Visited.Contains((y, x + 1)) && east.Any(c => c == Map[y][x]) && west.Any(c => c == Map[y][x + 1]))
+        if (x < m[0].Length - 1 && !Visited.Contains((y, x + 1)) && east.Any(c => c == m[y][x]) && west.Any(c => c == m[y][x + 1]))
         {
             Visited.Add((y, x + 1));
             q.Enqueue((y, x + 1));
+            
         }
 
     }
-    return Visited.Count / 2;
+        return Visited.Count / 2;
 }
+
+
 
 
