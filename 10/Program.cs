6b06b5399c6f03a2b0f51f6ed836187d3140cf50
Magnus Olofsson <magnusolofsson@outlook.com>
2023-12-14 11:04:07 +0100

    Solved Part 1 Day 10

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+string[] map = File.ReadAllLines("../data/10.dat");
+int Sx = 0, Sy = 0;
+for (int y = 0; y < map.Length; y++)
+{
+    for (int x = 0; x < map[0].Length; x++)
+    {
+        if (map[y][x] == 'S')
+        {
+            Sx = x;
+            Sy = y;
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {GetSteps(map, Sx, Sy)}");
+
+int GetSteps(string[] Map, int startX, int startY)
+{
+
+    Queue<(int, int)> q = new();
+    HashSet<(int, int)> Visited = new();
+    char[] north = { 'S', '|', 'L', 'J' };
+    char[] south = { 'S', '|', '7', 'F' };
+    char[] east = { 'S', '-', 'L', 'F' };
+    char[] west = { 'S', '-', 'J', '7' };
+
+    q.Enqueue((startY, startX));
+    Visited.Add((startY, startX));
+    while (q.Count > 0)
+    {
+        (int y, int x) = q.Dequeue();
+        // go north
+        if (y > 0 && !Visited.Contains((y - 1, x)) && north.Any(c => c == Map[y][x]) && south.Any(c => c == Map[y - 1][x]))
+        {
+            Visited.Add((y - 1, x));
+            q.Enqueue((y - 1, x));
+        }
+
+        // go south
+        if (y < Map.Length - 1 && !Visited.Contains((y + 1, x)) && south.Any(c => c == Map[y][x]) && north.Any(c => c == Map[y + 1][x]))
+        {
+            Visited.Add((y + 1, x));
+            q.Enqueue((y + 1, x));
+        }
+
+        // go west
+        if (x > 0 && !Visited.Contains((y, x - 1)) && west.Any(c => c == Map[y][x]) && east.Any(c => c == Map[y][x - 1]))
+        {
+            Visited.Add((y, x - 1));
+            q.Enqueue((y, x - 1));
+        }
+
+        // go east
+        if (x < Map[0].Length - 1 && !Visited.Contains((y, x + 1)) && east.Any(c => c == Map[y][x]) && west.Any(c => c == Map[y][x + 1]))
+        {
+            Visited.Add((y, x + 1));
+            q.Enqueue((y, x + 1));
+        }
+
+    }
+    return Visited.Count / 2;
+}
+
+
--- 10/README.md
+++ 10/README.md
@@ -1,1 +1,5 @@
 # Advent of Code 2023 Day10  
+
+## Part 1  
+
+So I waited a long time before starting this challenge. I got demotivated when reading it on the 10th, and did not pick it up again until today on the 14th. It was not that hard, just traverse the grid from the starting point in all the possible directions (depending on the shape of the tile, and the adjacent tile). And then count the number of steps in the loop, divide that by 2 to get the farthest step.
