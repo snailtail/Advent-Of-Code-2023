6b961cbfa914cd9218f32ad18ecf5fe37032bda0
Magnus Olofsson <magnusolofsson@outlook.com>
2023-12-16 16:08:41 +0100

    Solved part 1

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("../data/12.dat");
+ConditionRecord cr;
+long sum = 0;
+foreach(var line in lines)
+{
+    cr = new ConditionRecord(line);
+    sum+=cr.ValidSpringConditionCount;
+    
+}
+Console.WriteLine($"Part 1: {sum}");
